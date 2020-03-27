3cf70ce5fbd97d174385e9c04fbe1de4262d7012
Shreyas Bhavsar <44371717+shreyasb999@users.noreply.github.com>
2020-03-27 16:46:17 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+
+class friends:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    def predictionfriends(self):
+        # load model
+        model = load_model('model.h5')
+
+        # summarize model
+        #model.summary()
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = model.predict(test_image)
+
+      
+        if result[0][0]==1:
+            prediction='This is Omkar!'
+            return[prediction]
+        if result[0][1]==1:
+            prediction='This is Rishabh!'
+            return[prediction]
+        if result[0][2]==1:
+            prediction='This is Rutvik!'
+            return[prediction]
